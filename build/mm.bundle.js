5f2456877d485d2b30936364b5fbe747ad565bec
MohamadZahili <zahili@oum.edu.my>
2016-09-13 07:58:32 +0800

    Changes

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -6996,8 +6996,8 @@ angular.module('mm.core')
         priority: 500,
         compile: function(el, attrs) {
             attrs.$set('type',
-                null,               
-                false               
+                null,
+                false
             );
         }
     }
@@ -7589,7 +7589,7 @@ angular.module('mm.core.contentlinks', [])
         url: '/mm_contentlinks',
         abstract: true,
         templateUrl: 'core/components/contentlinks/templates/base.html',
-        cache: false,  
+        cache: false,
     })
     .state('mm_contentlinks.choosesite', {
         url: '/choosesite',
@@ -7748,7 +7748,7 @@ angular.module('mm.core.login', [])
         url: '/mm_login',
         abstract: true,
         templateUrl: 'core/components/login/templates/base.html',
-        cache: false,  
+        cache: false,
         onEnter: ["$ionicHistory", function($ionicHistory) {
             $ionicHistory.clearHistory();
         }]
@@ -26124,8 +26124,8 @@ angular.module('mm.addons.mod_glossary')
         limitFrom = 0,
         limitNum = 25,
         popover,
-        viewMode,  
-        fetchMode = 'letter_all',      
+        viewMode,
+        fetchMode = 'letter_all',
         fetchFunction,
         fetchInvalidate,
         fetchArguments,
@@ -31068,7 +31068,7 @@ angular.module('mm.addons.mod_resource')
 .directive('mmaModResourceHtmlLink', function() {
     return {
         restrict: 'A',
-        priority: 99,  
+        priority: 99,
         link: function(scope, element, attrs) {
             element.on('click', function(event) {
                 var href = element[0].getAttribute('data-href');
@@ -32241,7 +32241,7 @@ angular.module('mm.addons.mod_scorm')
             var cFirst = sFirst[2].split(".");
             var cSecond = sSecond[2].split(".");
             var change = 0;
-            FirstCents = 0; 
+            FirstCents = 0;
             if (cFirst.length > 1) {
                 FirstCents = parseInt(cFirst[1],10);
             }
@@ -32255,19 +32255,19 @@ angular.module('mm.addons.mod_scorm')
             if (Math.floor(cents) < 10) {
                 cents = "0" + cents.toString();
             }
-            var secs = parseInt(cFirst[0],10) + parseInt(cSecond[0],10) + change; 
+            var secs = parseInt(cFirst[0],10) + parseInt(cSecond[0],10) + change;
             change = Math.floor(secs / 60);
             secs = secs - (change * 60);
             if (Math.floor(secs) < 10) {
                 secs = "0" + secs.toString();
             }
-            mins = parseInt(sFirst[1],10) + parseInt(sSecond[1],10) + change;  
+            mins = parseInt(sFirst[1],10) + parseInt(sSecond[1],10) + change;
             change = Math.floor(mins / 60);
             mins = mins - (change * 60);
             if (mins < 10) {
                 mins = "0" + mins.toString();
             }
-            hours = parseInt(sFirst[0],10) + parseInt(sSecond[0],10) + change; 
+            hours = parseInt(sFirst[0],10) + parseInt(sSecond[0],10) + change;
             if (hours < 10) {
                 hours = "0" + hours.toString();
             }
@@ -33287,7 +33287,7 @@ angular.module('mm.addons.mod_scorm')
                     score = attemptscore.scos;
                 break;
                 default:
-                    score = attemptscore.max;  
+                    score = attemptscore.max;
             }
             return score;
         });
@@ -37513,16 +37513,17 @@ angular.module('mm.addons.mod_quiz')
 angular.module('mm.core')
 
 .constant('mmCoreConfigConstants', {
-    "app_id" : "com.moodle.moodlemobile",
+    "app_id" : "com.oum.myinspire",
     "versioncode" : "2014",
-    "versionname" : "3.1.2",
+    "versionname" : "3.1.21",
     "cache_expiration_time" : 300000,
     "default_lang" : "en",
     "languages": {"ar": "عربي", "bg": "Български", "ca": "Català", "cs": "Čeština", "da": "Dansk", "de": "Deutsch","en": "English", "es": "Español", "es-mx": "Español - México", "eu": "Euskara", "fa": "فارسی", "fr" : "Français", "he" : "עברית", "hu": "magyar", "it": "Italiano", "ja": "日本語","nl": "Nederlands", "pl": "Polski", "pt-br": "Português - Brasil", "pt": "Português - Portugal", "ro": "Română", "ru": "Русский", "sv": "Svenska", "tr" : "Türkçe", "zh-cn" : "简体中文", "zh-tw" : "正體中文"},
     "wsservice" : "moodle_mobile_app",
     "wsextservice" : "local_mobile",
-    "demo_sites": {"student": {"url": "http://school.demo.moodle.net", "username": "student", "password": "moodle"}, "teacher": {"url": "http://school.demo.moodle.net", "username": "teacher", "password": "moodle"}, "cva": {"url": "http://mm.cvaconsulting.com/moodle", "username": "student", "password": "student"}},
-    "gcmpn": "694767596569",
+    "demo_sites": "",
+    "siteurl":"http://inspire.oum.edu.my",
+    "gcmpn": "353552729607",
     "customurlscheme": "moodlemobile"
 }
 );
